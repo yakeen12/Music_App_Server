ad53d5cf73f29577d824a55db9a02755abc2b1cb
yakeen12 <yakeensabha@gmail.com>
2024-12-31 00:41:17 +0300

    m

--- Controllers/playListsController.js
+++ Controllers/playListsController.js
@@ -31,11 +31,10 @@ exports.getUserPlaylists = async (req, res) => {
 
     try {
 
-        const playlists = await Playlist.find({ createdBy: req.user.userId })
-            .populate({
-                path: 'songs',
-                //     populate: { path: 'artist', }
-            });
+        const playlists = await Playlist.find({ createdBy: req.user.userId }).populate({
+            path: 'songs',
+            populate: { path: 'artist', select: 'name' }
+        });
         console.log("getUserPlaylists playlists", playlists);
         if (!playlists || playlists.length === 0) {
             return res.status(404).json({ message: 'No playlists found for this user' });
