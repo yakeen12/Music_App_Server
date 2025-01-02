cd9622d0cfb185a546385f5de8cf258da3bfb541
yakeen12 <yakeensabha@gmail.com>
2025-01-02 20:50:53 +0300

    hj

--- Routes/episodeRoutes.js
+++ Routes/episodeRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const episodeController = require('../Controllers/episodeController'); // Assuming this file is saved as episodeController.js
 
-router.get('/', episodeController.getAllEpisodes); // Route to get all episodes
 router.get('/latest', episodeController.getLatestEpisodes); // Route to get the latest 10 episodes
+
+router.get('/', episodeController.getAllEpisodes); // Route to get all episodes
 router.post('/', episodeController.createEpisode); // Route to create a new episode
 
 module.exports = router;
