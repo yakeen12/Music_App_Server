84b303d7a266ba873db40e96fd518fdf4b71bd91
yakeen12 <yakeensabha@gmail.com>
2025-01-03 19:12:15 +0300

    i've spent 4 hours on this mf

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -127,7 +127,7 @@ exports.getPostById = async (req, res) => {
                 path: 'episode',
                 populate: {
                     path: "podcast",
-                    select: "title"
+                    select: "title, img"
                 }
             })  // استرجاع تفاصيل البودكاست (إذا موجود)
             .sort({ createdAt: -1 });  // ترتيب البوستات بناءً على التاريخ (الأحدث أولاً)
