74084fa4fd50572afe9c13747c5769384ea5773b
yakeen12 <yakeensabha@gmail.com>
2025-01-03 10:29:14 +0300

    wth is wrong w me

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -5,13 +5,13 @@ const postController = require('../Controllers/postController');
 
 
 //لايك للبوست
-router.post('/post/:postId/like', authenticate, postController.toggleLike);
+router.post('/:postId/like', authenticate, postController.toggleLike);
 
 // إضافة منشور
 router.post('/add', authenticate, postController.createPost);
 
 // by community
-router.get('/post/:communityName:', authenticate, postController.getPostsByCommunity)
+router.get('/:communityName', authenticate, postController.getPostsByCommunity)
 
 // مسار للحصول على منشور حسب ID
 router.get('/:postId', authenticate, postController.getPostById);
