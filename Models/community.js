4dccbd18e1e28f0ce7880f6703792411fcf3f4eb
yakeen12 <yakeensabha@gmail.com>
2025-01-03 04:06:02 +0300

    fix community

--- Models/community.js
+++ Models/community.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const communitySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+    }],
+
 });
 
 const Community = mongoose.model('Community', communitySchema);
 module.exports = Community;
+
--- Routes/communityRoutes.js
+++ Routes/communityRoutes.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const communityController = require('../Controllers/communityController');
-const authenticate = require('../MiddleWare/authenticate');  // للمصادقة
 
 // إنشاء كوميونيتي جديدة
-router.post('/create', authenticate, communityController.createCommunity);
- 
-// الانضمام إلى كوميونيتي
-router.post('/join', authenticate, communityController.joinCommunity);
-
-// عرض جميع الكوميونيتيز
-router.get('/', authenticate, communityController.getAllCommunities);
-
-// عرض تفاصيل كوميونيتي
-router.get('/:communityId', authenticate, communityController.getCommunityDetails);
-
-// إضافة عضو إلى كوميونيتي
-router.post('/addMember', authenticate, communityController.addMemberToCommunity);
+router.post('/create', communityController.createCommunity);
 
 module.exports = router;
