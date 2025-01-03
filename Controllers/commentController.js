24c446964e235e0a8cdd39bc6134e7bd98280b02
yakeen12 <yakeensabha@gmail.com>
2025-01-03 21:39:31 +0300

    forgot smth

--- Controllers/commentController.js
+++ Controllers/commentController.js
@@ -42,7 +42,7 @@ exports.getCommentsForPost = async (req, res) => {
     const { postId } = req.params;
 
     try {
-        const comments = await Comment.find({ post: postId }).populate('user', 'name');
+        const comments = await Comment.find({ post: postId }).populate('user', 'name profilePicture');
         res.status(200).json(comments);
     } catch (err) {
         res.status(500).json({ message: 'Server error' });
@@ -53,27 +53,27 @@ exports.getCommentsForPost = async (req, res) => {
 // ميثود لإضافة أو إزالة لايك للكومنتر
 exports.likeComment = async (req, res) => {
     try {
-      const userId = req.user._id; // الحصول على الـ userId من الـ token
-      const commentId = req.params.id; // الحصول على الـ commentId من الـ URL parameter
-  
-      const comment = await Comment.findById(commentId); // العثور على الكومنتر بناءً على الـ commentId
-      if (!comment) {
-        return res.status(404).send({ error: 'Comment not found' });
-      }
-  
-      // إذا كان المستخدم قد أعطى لايك للكومنتر مسبقاً، نقوم بإزالته
-      if (comment.likes.includes(userId)) {
-        comment.likes = comment.likes.filter(like => like.toString() !== userId.toString()); // إزالة الـ userId من likes
+        const userId = req.user._id; // الحصول على الـ userId من الـ token
+        const commentId = req.params.id; // الحصول على الـ commentId من الـ URL parameter
+
+        const comment = await Comment.findById(commentId); // العثور على الكومنتر بناءً على الـ commentId
+        if (!comment) {
+            return res.status(404).send({ error: 'Comment not found' });
+        }
+
+        // إذا كان المستخدم قد أعطى لايك للكومنتر مسبقاً، نقوم بإزالته
+        if (comment.likes.includes(userId)) {
+            comment.likes = comment.likes.filter(like => like.toString() !== userId.toString()); // إزالة الـ userId من likes
+            await comment.save();
+            return res.status(200).send({ message: 'Like removed' });
+        }
+
+        // إذا لم يكن قد أعطى لايك مسبقاً، نقوم بإضافته
+        comment.likes.push(userId);
         await comment.save();
-        return res.status(200).send({ message: 'Like removed' });
-      }
-  
-      // إذا لم يكن قد أعطى لايك مسبقاً، نقوم بإضافته
-      comment.likes.push(userId);
-      await comment.save();
-  
-      res.status(200).send({ message: 'Like added', comment });
+
+        res.status(200).send({ message: 'Like added', comment });
     } catch (error) {
-      res.status(500).send({ error: 'Failed to add/remove like' });
+        res.status(500).send({ error: 'Failed to add/remove like' });
     }
-  };
+};
