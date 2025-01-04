8c095d59a2ef40da0a2b263b6c56e416eaeee536
yakeen12 <yakeensabha@gmail.com>
2025-01-04 20:25:27 +0300

    kndc

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -4,20 +4,20 @@ const router = express.Router();
 const postController = require('../Controllers/postController');
 
 
-//لايك للبوست
-router.post('/:postId/like', authenticate, postController.toggleLike);
-
 // إضافة منشور
 router.post('/add', authenticate, postController.createPost);
 
-// by community
-router.get('/:communityName', authenticate, postController.getPostsByCommunity)
-
-// مسار للحصول على منشور حسب ID
-router.get('/:postId', authenticate, postController.getPostById);
-
 // عرض جميع المنشورات
 router.get('/', authenticate, postController.getAllPosts);
+
+// الحصول على منشور حسب ID (مسار محدد أكثر من اسم المجتمع)
+router.get('/post/:postId', authenticate, postController.getPostById);
+
+// المنشورات حسب المجتمع
+router.get('/:communityName', authenticate, postController.getPostsByCommunity);
+
+// لايك للبوست
+router.post('/:postId/like', authenticate, postController.toggleLike);
 
 
 
