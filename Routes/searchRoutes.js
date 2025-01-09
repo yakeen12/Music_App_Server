5197c1da881597679eb0bebc29aad3b1b87318b0
yakeen12 <yakeensabha@gmail.com>
2025-01-09 21:02:31 +0300

    what?

--- Routes/searchRoutes.js
+++ Routes/searchRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const SearchController = require('../Controllers/searchController');  // تأكد من المسار الصحيح
 
+
+
+router.get('/searchsongs', SearchController.search);
+
 // راوت البحث مع التجزئة
 router.get('/search', SearchController.search);
 
 
-router.get('/searchsongs', SearchController.search);
 
 module.exports = router;
